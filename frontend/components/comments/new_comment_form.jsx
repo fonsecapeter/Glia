38cb1d44fe93f4708eaf9cca09cfa3c8177624c3
fonseapeter <peter.nfonseca@gmail.com>
2016-07-06 11:47:26 -0700

    working on comment form

--- frontend/components/comments/new_comment_form.jsx
+++ frontend/components/comments/new_comment_form.jsx
@@ -1,12 +1,26 @@
 const React = require('react');
 
 const NewCommentForm = React.createClass({
+  getInitialState () {
+    return ({
+      content: ''
+    });
+  },
+
+  _onContentChange (event) {
+    this.setState({ content: event.currentTarget.value });
+  },
+
   render () {
     return (
       <form onSubmit={ this._onSubmit }>
-
+        <input
+          className="comment-input"
+          type="text"
+          onChange={ this._onContentChange }
+          value={ this.state.content } />
+        <input type="submit" className="button comment-submit" value="comment" />
       </form>
-      <p>new comment form</p>
     );
   }
 });
