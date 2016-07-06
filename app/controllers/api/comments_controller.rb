1c32c894b99ed71d4830ae8dc29efd4350da44ad
fonseapeter <peter.nfonseca@gmail.com>
2016-07-05 18:16:22 -0700

    found the source of error! thank you heroku rails console

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,19 +1,19 @@
 class Api::CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
-    @comment.commentable_id = params[:comment][:commentable_id]
-    @comment.commentable_type = params[:comment][:commentable_type]
+    # @comment = Comment.new(comment_params)
+    # @comment.commentable_id = params[:comment][:commentable_id]
+    # @comment.commentable_type = params[:comment][:commentable_type]
+    #
+    # if (current_user)
+    #   @comment.author_id = current_user.idea
+    # else
+    #   @comment.author_id = 1
+    # end
 
-    if (current_user)
-      @comment.author_id = current_user.idea
-    else
-      @comment.author_id = 1
-    end
-
-    if (commentable_type = 'Question')
-
-    if @comment.save
-      @user = current_user
+    # if (commentable_type = 'Question')
+    #
+    # if @comment.save
+    #   @user = current_user
 
   end
 
