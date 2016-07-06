9b45eff6d769f9bf54c5b5643761a49c66c48c9c
fonseapeter <peter.nfonseca@gmail.com>
2016-07-06 11:34:21 -0700

    trying larger header font for mobile

--- frontend/components/comments/new_comment_form.jsx
+++ frontend/components/comments/new_comment_form.jsx
@@ -3,6 +3,9 @@ const React = require('react');
 const NewCommentForm = React.createClass({
   render () {
     return (
+      <form onSubmit={ this._onSubmit }>
+
+      </form>
       <p>new comment form</p>
     );
   }
