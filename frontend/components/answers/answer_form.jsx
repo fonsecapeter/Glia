9658ba772dceb50bfeebacb44a956147099f06f9
fonseapeter <peter.nfonseca@gmail.com>
2016-07-08 11:13:15 -0700

    users can now insert image via dropzone and upload to cloudinary

--- frontend/components/answers/answer_form.jsx
+++ frontend/components/answers/answer_form.jsx
@@ -1,18 +1,25 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const ReactQuill = require('react-quill');
+const Dropzone = require('react-dropzone');
 const AnswerActions = require('../../actions/answer_actions');
 const hashHistory = require('react-router').hashHistory;
+const cloudinaryConfig = require('react-cloudinary').cloudinaryConfig;
+const CloudinaryImage = require('react-cloudinary').CloudinaryImage;
+cloudinaryConfig({ cloud_name: 'dxhqr7u1z' });
 
 const AnswerCreateForm = React.createClass({
   getInitialState () {
     if (this.props.method === 'create') {
       return ({
         content: '',
+        file: '',
+        disabledState: false
       });
     } else if (this.props.method === 'edit') {
       return ({
-        content: this.props.content
+        content: this.props.content,
+        file: ''
       });
     }
   },
@@ -44,6 +51,50 @@ const AnswerCreateForm = React.createClass({
     this.props.closeSelf();
   },
 
+  onDrop (files) {
+    console.log(files[0].preview);
+    this.setState({
+      file: files[0],
+      disabledState: true,
+     });
+
+    const url = "https://api.cloudinary.com/v1_1/dxhqr7u1z/image/upload";
+    const timestamp = Date.now() / 1000 | 0;
+    const apiKey = 627249736112177;
+
+    const self = this;
+
+    const formData = new FormData();
+    formData.append('file', files[0]);
+    formData.append('timestamp', timestamp);
+    formData.append('api_key', apiKey);
+    formData.append('upload_preset', 'scjzwdf9');
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      cache: false,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+      success: xhr => {
+        console.log(xhr);
+        // const image = `<CloudinaryImage className="img" publicId=${xhr.public_id} />`;
+        const image = `<img src=\"http://res.cloudinary.com/dxhqr7u1z/image/fetch/${ xhr.url }\" /><br />`;
+        const cont = self.state.content;
+        debugger;
+        self.setState({
+          content: image + cont,
+          disabledState: false
+        });
+      },
+      error: function (xhr) {
+        console.log(xhr);
+      }
+    });
+  },
+
   render () {
     let buttonText;
     if (this.props.method === 'create') {
@@ -52,17 +103,40 @@ const AnswerCreateForm = React.createClass({
       buttonText = 'edit';
     }
 
+    let disabledClass = '';
+    if (this.state.disabledState) {
+      disabledClass = ' disabled';
+    }
+
     return (
       <div>
         <form onSubmit={ this._onSubmit }>
           <ReactQuill
-            className="quill"
+            className={ "quill" + disabledClass }
             theme="snow"
             value={ this.state.content }
-            onChange={ this._onContentChange } />
+            onChange={ this._onContentChange }
+            disabled={ this.state.disabledState } />
 
           <br /><br />
-          <input type="submit" className="button" value={ buttonText } />
+
+          <Dropzone
+            className={ "dropzone" + disabledClass }
+            onDrop={ this.onDrop }
+            multiple={ false }
+            accept="image/png,image/jpeg,image/gif"
+            disabled={ this.state.disabledState }>
+            <p>Drag an image from file</p>
+            <img src={ this.state.file.preview } />
+          </Dropzone>
+
+          <br /><br />
+
+          <input
+            type="submit"
+            className={ "button" + disabledClass }
+            value={ buttonText }
+            disabled={ this.state.disabledState } />
         </form>
       </div>
     );
